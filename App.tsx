3911f90f94d8430d45eb03d051b7e4a1911a8eaf
jswj <jswj30@naver.com>
2022-10-26 17:07:13 +0900

    1

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import React, {useEffect} from 'react';
-import {Text, View, SafeAreaView, Platform} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, SafeAreaView, Platform} from 'react-native';
 import AppleHealthKit, {
   HealthValue,
   HealthKitPermissions,
@@ -14,6 +14,8 @@ const permissions = {
 } as HealthKitPermissions;
 
 const App = () => {
+  const [steps, setSteps] = useState('fffadf');
+
   useEffect(() => {
     if (Platform.OS === 'ios') {
       AppleHealthKit.initHealthKit(permissions, (error: string) => {
@@ -23,31 +25,21 @@ const App = () => {
           console.log('[ERROR] Cannot grant permissions!');
         }
 
-        /* Can now read or write to HealthKit */
-
-        // const options = {
-        //   startDate: new Date(2020, 1, 1).toISOString(),
-        // };
-
         let options = {
           // date: new Date().toISOString(), // optional; default now
-          includeManuallyAdded: true, // optional: default true
+          startDate: new Date(2022, 9, 24).toISOString(),
+          date: new Date(2022, 9, 26).toISOString(),
+          includeManuallyAdded: false, // optional: default true
         };
-
-        // AppleHealthKit.getHeartRateSamples(
-        //   options,
-        //   (callbackError: string, results: HealthValue[]) => {
-        //     /* Samples are now collected from HealthKit */
-        //   },
-        // );
 
         AppleHealthKit.getStepCount(
           options,
           (err: Object, results: HealthValue) => {
             if (err) {
+              console.log(err);
               return;
             }
-            console.log('results: ', results);
+            console.log(results);
           },
         );
       });
@@ -56,8 +48,14 @@ const App = () => {
 
   return (
     <SafeAreaView>
-      <View>
-        <Text>App</Text>
+      <View
+        style={{
+          width: '100%',
+          height: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <Text style={{fontSize: 30}}>{steps}</Text>
       </View>
     </SafeAreaView>
   );
