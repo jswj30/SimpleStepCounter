788728cdd53817aa8f9be8df5b7661a8707b31cf
jswj <jswj30@naver.com>
2022-11-08 15:17:14 +0900

    reinit

--- App.tsx
+++ App.tsx
@@ -1,7 +1,46 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import AppleHealthKit, {
+  HealthValue,
+  HealthKitPermissions,
+} from 'react-native-health';
+
+/* Permission options */
+const permissions = {
+  permissions: {
+    read: [AppleHealthKit.Constants.Permissions.HeartRate],
+    write: [AppleHealthKit.Constants.Permissions.Steps],
+  },
+} as HealthKitPermissions;
 
 const App = () => {
+  AppleHealthKit.initHealthKit(permissions, (error: string) => {
+    /* Called after we receive a response from the system */
+
+    if (error) {
+      console.log('[ERROR] Cannot grant permissions!');
+    }
+
+    /* Can now read or write to HealthKit */
+
+    const options = {
+      startDate: new Date(2022, 11, 7).toISOString(),
+    };
+
+    AppleHealthKit.getHeartRateSamples(
+      options,
+      (callbackError: string, results: HealthValue[]) => {
+        /* Samples are now collected from HealthKit */
+
+        console.log('results=====');
+        console.log(results);
+
+        console.log('callbackError===');
+        console.log(callbackError);
+      },
+    );
+  });
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>SimpleStepCounter</Text>
--- ios/SimpleStepCounter/AppDelegate.mm
+++ ios/SimpleStepCounter/AppDelegate.mm
@@ -6,6 +6,9 @@
 
 #import <React/RCTAppSetupUtils.h>
 
+/* Add the library import at the top of AppDelegate.m */
+#import "RCTAppleHealthKit.h"
+
 #if RCT_NEW_ARCH_ENABLED
 #import <React/CoreModulesPlugins.h>
 #import <React/RCTCxxBridgeDelegate.h>
@@ -57,6 +60,10 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+
+  /* Add Background initializer for HealthKit  */
+  [[RCTAppleHealthKit new] initializeBackgroundObservers:bridge];
+
   return YES;
 }
 
