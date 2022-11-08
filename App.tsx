aca346a6a761789c438f5fb4a9f36f6a72cd548a
jswj <jswj30@naver.com>
2022-11-08 14:19:58 +0900

    1

--- App.tsx
+++ App.tsx
@@ -1,64 +1,24 @@
-import React, {useState, useEffect} from 'react';
-import {View, Text, SafeAreaView, Platform} from 'react-native';
-import AppleHealthKit, {
-  HealthValue,
-  HealthKitPermissions,
-} from 'react-native-health';
-
-/* Permission options */
-const permissions = {
-  permissions: {
-    read: [AppleHealthKit.Constants.Permissions.HeartRate],
-    write: [AppleHealthKit.Constants.Permissions.Steps],
-  },
-} as HealthKitPermissions;
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 
 const App = () => {
-  const [steps, setSteps] = useState('fffadf');
-
-  useEffect(() => {
-    if (Platform.OS === 'ios') {
-      AppleHealthKit.initHealthKit(permissions, (error: string) => {
-        /* Called after we receive a response from the system */
-
-        if (error) {
-          console.log('[ERROR] Cannot grant permissions!');
-        }
-
-        let options = {
-          // date: new Date().toISOString(), // optional; default now
-          startDate: new Date(2022, 9, 24).toISOString(),
-          date: new Date(2022, 9, 26).toISOString(),
-          includeManuallyAdded: false, // optional: default true
-        };
-
-        AppleHealthKit.getStepCount(
-          options,
-          (err: Object, results: HealthValue) => {
-            if (err) {
-              console.log(err);
-              return;
-            }
-            console.log(results);
-          },
-        );
-      });
-    }
-  }, []);
-
   return (
-    <SafeAreaView>
-      <View
-        style={{
-          width: '100%',
-          height: '100%',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}>
-        <Text style={{fontSize: 30}}>{steps}</Text>
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text style={styles.text}>SimpleStepCounter</Text>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 30,
+    color: '#000',
+  },
+});
 
 export default App;
